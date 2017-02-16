b892ea04b14bae9d0fb8d47de4a145d1c536eebf
Phil Watts <phil@hashicorp.com>
2017-02-16 14:02:14 -0500

    updated outputs

--- interface.tf
+++ interface.tf
@@ -8,6 +8,6 @@ variable "subnet_bit" {}
 
 
 output "network_id" { value = "${azurerm_virtual_network.network.id}" }
+output "network_name" { value = "${var.network_name}" }
 output "priv_subnets" { value = ["${azurerm_subnet.priv.*.id}"] }
 output "pub_subnets" { value = ["${azurerm_subnet.pub.*.id}"] }
-
